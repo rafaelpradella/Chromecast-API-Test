f32df5ef620b1a6a73f6656023273d58ca823fd8
Rafael Pradella <pradella@outlook.com>
2014-10-12 00:33:27 -0300

    Maybe WEBM wasn't the best solution ¯\_(ツ)_/¯, let's go back to MP4 with 9MB

--- js/main.js
+++ js/main.js
@@ -78,9 +78,9 @@ $( document ).ready(function(){
 	   					return;
 	   			}
 	   			// define URL of media
-	   			var mediaInfo = new chrome.cast.media.MediaInfo('http://rafaelpradella.github.io/media/message.webm');
+	   			var mediaInfo = new chrome.cast.media.MediaInfo('http://rafaelpradella.github.io/media/message.mp4');
 	   			// contentType : MIME type
-	   			mediaInfo.contentType = 'video/webm';
+	   			mediaInfo.contentType = 'video/mp4';
 
 	   			// Request load of the content and set autoplay to true
 	   			var request = new chrome.cast.media.LoadRequest(mediaInfo);
