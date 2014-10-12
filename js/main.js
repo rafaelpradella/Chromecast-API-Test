bce1b9f0adbada9fd1afabdcb36f3923ebf73843
Rafael Pradella <pradella@outlook.com>
2014-10-11 23:15:39 -0300

    Functions of initializeCastApi fully declared

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ var session = null; // define status of Chromecast Session
 
 $( document ).ready(function(){
 	    
-		// Ping Chromecast
+		// Ping API
 	    var loadCastInterval = setInterval(function(){
 	            if (chrome.cast.isAvailable) {
 	                    $('#castme').contents().last().replaceWith("Transmitir");
 	                    clearInterval(loadCastInterval);
 	                    initializeCastApi();
 	            } else {
-	                    $('#castme').css("display","none"); // Hide button if device is unreachable
+	                    $('#castme').css("display","none"); // Hide button if API is unreachable
 	            }
 	    }, 1000);
 
@@ -22,11 +22,32 @@ $( document ).ready(function(){
 	    	chrome.cast.initialize(apiConfig, onInitSuccess, onInitError);
 	    };
 
+	    // In case of chrome.cast.initialize passed
+	   	function onInitSuccess() {
+	   		console.log('Initialization succedded');
+	   	}
+
+	   	// In case of chrome.cast.initialize failed
+	   	function onInitError() {
+	   		console.log('Initialization failed :~~(');
+	   	}
+
+	   	// Listen if Chromecast is reachable	
+	    function receiverListener(e){
+	    	if (e === 'available') {
+	    		console.log('Chromecast was found on the network')
+	    	}
+
+	    	else {
+	    		console.log ('There are no Chromecasts available');
+	    	}
+	    };
+
 	   	function sessionListener (e) {
 	   		session = e; // defines session status by event
 	   		console.log('New Session');
 	   		if (session.media.length != 0) { // Log if session has media
 	   			console.log('Found'+ session.media.length + 'sessions.'); 
 	   		}
-	   	}
+	   	};
 });
