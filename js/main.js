45bb9b7156957920047feac62ee99bd3d9a53807
Rafael Pradella <pradella@outlook.com>
2014-10-11 22:49:36 -0300

    Start API calls and SessionListener

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
+
+var session = null; // define status of Chromecast Session
+
 $( document ).ready(function(){
+	    
+		// Ping Chromecast
 	    var loadCastInterval = setInterval(function(){
 	            if (chrome.cast.isAvailable) {
-	                    console.log('Cast has loaded.');
+	                    $('#castme').contents().last().replaceWith("Transmitir");
 	                    clearInterval(loadCastInterval);
 	                    initializeCastApi();
 	            } else {
-	                    console.log('Unavailable');
+	                    $('#castme').css("display","none"); // Hide button if device is unreachable
 	            }
 	    }, 1000);
+
+	    // initializeCastApi: Load the Chromecast API
+	    function initializeCastApi(){
+	    	var applicationID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID; // Pick device ID
+	    	var sessionRequest = new chrome.cast.SessionRequest(applicationID); // request session in identified (by APP_ID) device
+	    	var apiConfig = new chrome.cast.ApiConfig(sessionRequest,sessionListener,receicerListener); 
+	    	chrome.cast.initialize(apiConfig, onInitSuccess, onInitError);
+	    };
+
+	   	function sessionListener (e) {
+	   		session = e; // defines session status by event
+	   		console.log('New Session');
+	   		if (session.media.length != 0) { // Log if session has media
+	   			console.log('Found'+ session.media.length + 'sessions.'); 
+	   		}
+	   	}
 });
